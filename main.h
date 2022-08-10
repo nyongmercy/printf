14689bc0ae99bb94adc6a02bc76bc7b13e256ef2
Edem <edemoski.eu@gmail.com>
2022-08-09 21:03:05 -0700

    main.h correction

--- main.h
+++ main.h
@@ -71,7 +71,7 @@ int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
-int print_non_printable(va_list types, char buffer[]
+int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Function to print memory address */
@@ -85,7 +85,7 @@ int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
 /*Function to print string in reverse*/
-int print_reverse(va_list types, char buffer[].
+int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /*Function to print a string in rot 13*/
